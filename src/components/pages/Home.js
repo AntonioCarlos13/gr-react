952a76ead545d54c0179e988f0e6c2926a12c574
Antonio Carlos <antonio.rodrigues.0058@gmail.com>
2022-10-17 17:14:24 -0300

    ADICIONADO A SEÇÃO HOME

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,15 @@
+import styles from './Home.module.css'
+import porco from '../../img/porco.png'
+import LinkButton from '../layout/LinkButton'
+
 function Home(){
     return(
-        <h1>Home</h1>
+        <section className={styles.home_container}>
+            <h1>Bem-vindo ao <span>GR</span></h1>
+            <p>Começe a gerenciar os seus projetos agora mesmo!</p>
+            <LinkButton to="/novoProjeto" text="Criar Projeto" />
+            <img src={porco} alt="GR"/>
+        </section>
     )
 }
 
