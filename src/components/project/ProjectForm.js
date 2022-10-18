ec616af5f6dc84ab38cfb029de6ca52e1bc51c0a
Antonio Carlos <antonio.rodrigues.0058@gmail.com>
2022-10-18 11:13:57 -0300

    CONEXÃO COM API

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,31 +1,53 @@
-import Input from "../form/Input"
-import Select from "../form/Select"
-import SubmitButton from "../form/SubmitButton"
-import styles from "./ProjectForm.module.css"
+import { useEffect, useState } from "react";
+
+import Input from "../form/Input";
+import Select from "../form/Select";
+import SubmitButton from "../form/SubmitButton";
+import styles from "./ProjectForm.module.css";
 
 function ProjectForm({ btnText }) {
-    return (
-        <form className={styles.form}>
-            <Input
-                type="text" 
-                text="Nome do projeto" 
-                name="name" 
-                placeholder="Insira o nome do projeto" />
-            
-            <Input
-                type="number" 
-                text="Orçamento" 
-                name="budget" 
-                placeholder="Insira o orçamento total" />
+  const [categories, setCategories] = useState([]);
 
-            <Select 
-                name="category_id"
-                text="Selecione a categoria" />
+  useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((resp) => resp.json())
+    .then((data) => {
+      setCategories(data);
+    })
+    .catch((err) => console.log(err));
 
-            
-            <SubmitButton text={btnText} />
-        </form>
-    )
+  }, [])
+
+  return (
+    <form className={styles.form}>
+      <Input
+        type="text"
+        text="Nome do projeto"
+        name="name"
+        placeholder="Insira o nome do projeto"
+      />
+
+      <Input
+        type="number"
+        text="Orçamento"
+        name="budget"
+        placeholder="Insira o orçamento total"
+      />
+
+      <Select
+        name="category_id"
+        text="Selecione a categoria"
+        options={categories}
+      />
+
+      <SubmitButton text={btnText} />
+    </form>
+  );
 }
 
-export default ProjectForm
+export default ProjectForm;
